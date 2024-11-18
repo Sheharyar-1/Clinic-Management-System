1b9dceb2cd943474a834c45dec90f6802b376e02
Sheharyar-1 <sheharyar.ahmed@xprolabs.com>
2024-11-18 19:16:24 +0500

    Issues fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_13_074416) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_18_102155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,19 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_13_074416) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "appointments", force: :cascade do |t|
+    t.bigint "admin_id", null: false
+    t.bigint "patient_id", null: false
+    t.datetime "appointment_date", null: false
+    t.string "status", default: "upcoming", null: false
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email"
+    t.index ["admin_id"], name: "index_appointments_on_admin_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
+
   create_table "patients", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -37,4 +50,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_13_074416) do
     t.index ["email"], name: "index_patients_on_email", unique: true
     t.index ["reset_password_token"], name: "index_patients_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "appointments", "admins"
+  add_foreign_key "appointments", "patients"
 end
