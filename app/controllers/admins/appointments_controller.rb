1cfe1aa45d81c8b44b5aae84fcd3c41d16b90d5b
Sheharyar-1 <sheharyar.ahmed@xprolabs.com>
2024-11-19 18:08:38 +0500

    Styling the interphase

--- app/controllers/admins/appointments_controller.rb
+++ app/controllers/admins/appointments_controller.rb
@@ -1,20 +1,15 @@
 class Admins::AppointmentsController < ApplicationController
-  before_action :authenticate_admin!
+  before_action :authenticate_admin!, :find_appointment
 
   def index
     @appointments = Appointment.all
   end
 
-  def show
-    @appointment = Appointment.find(params[:id])
-  end
+  def show;end
 
-  def edit
-    @appointment = Appointment.find(params[:id])
-  end
+  def edit;end
 
   def update
-    @appointment = Appointment.find(params[:id])
     if @appointment.update(appointment_params)
       redirect_to admin_appointment_path(@appointment), notice: "Appointment updated successfully."
     else
@@ -23,12 +18,15 @@ class Admins::AppointmentsController < ApplicationController
   end
 
   def destroy
-    @appointment = Appointment.find(params[:id])
     @appointment.destroy
     redirect_to admin_appointments_path, notice: "Appointment deleted successfully."
   end
 
   private
+
+  def find_appointment
+    @appointment = Appointment.find(params[:id])
+  end
 
   def appointment_params
     params.require(:appointment).permit(:appointment_date, :status, :notes, :doctor_id, :patient_id)
